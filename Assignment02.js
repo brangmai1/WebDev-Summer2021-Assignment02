cd1f1665b1ca44f0e3669e61cf99b74d2ba42055
Brang Mai <brangmai100@gmail.com>
2021-06-10 13:40:33 -0400

    added more functionalities of Assignment02.js

--- Assignment02.js
+++ Assignment02.js
@@ -1,17 +1,31 @@
-// FOR EACH //
-Array.prototype.myEach = function() {
-
+/* FOR EACH 
+ * This function takes an array of integer and a funcion to print out as arguments
+ * Then prints all the elements of the array
+ */
+function forEach(array, operation){
+    for(let i = 0; i < array.length; i++){
+        operation(i);
+    }
 };
+
+forEach([11, 22, 33, 44, 55], console.log);
 
 // MAP //
-Array.prototype.myMap = function() {
+//Array.prototype.myMap = function() {
+function myMap(array){
+    
 
 };
 
-// FILTER //
-Array.prototype.myFilter = function() {
-
+/* FILTER 
+ * This function takes an integer array as an arguement
+ * It then returns even numbers
+ */
+function myFilter(array){
+    return array.filter(element => (element % 2 === 0));    
 };
+
+console.log(myFilter([11, 22, 33, 44, 55]));
 
 // SOME //
 Array.prototype.mySome = function() {
