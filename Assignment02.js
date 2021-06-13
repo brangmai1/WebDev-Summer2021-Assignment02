d86999209ce510d8fbfc1945269eb0ac6b2177ac
Brang Mai <brangmai100@gmail.com>
2021-06-12 22:36:46 -0400

    implemented more functions

--- Assignment02.js
+++ Assignment02.js
@@ -3,16 +3,14 @@
  * This function takes an array of integer and a funcion to print out as arguments
  * Then prints all the elements of the array
  */
-function forEach(array, operation){
+let forEach = function(array, operation){
     for(let i = 0; i < array.length; i++){
         operation(i);
     }
 };
-
 forEach([11, 22, 33, 44, 55], console.log);
 
 // MAP //
-//Array.prototype.myMap = function() {
 let square = function(number){
     return number * number;
 };
@@ -24,34 +22,64 @@ let myMap = function(array, callBack){
     return newArray;
 };
 console.log(myMap([11,22, 33, 44, 55, 66], square));
-/* 
-let arr = [11, 22, 33, 44, 55, 66, 77, 88, 99];
-let myMap = arr.map(element => element + element);
-console.log(myMap[11, 22, 33, 44, 55, 66, 77, 88]);
- */
+
 /* FILTER 
  * This function takes an integer array as an arguement
  * It then returns even numbers
  */
-function myFilter(array){
-    return array.filter(element => (element % 2 === 0));    
+let even = function(number){
+    return number % 2 == 0 ? 1 : 0;
+};
+let myFilter = function(array, even){
+    let newArray = [];
+    for(let i = 0; i < array.length; i++){
+        if(even(array[i])){
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;   
 };
 
 console.log(myFilter([11, 22, 33, 44, 55]));
 
 // SOME //
-Array.prototype.mySome = function() {
+// Array.prototype.mySome = function() {
+let  = function(number){
 
+}
+let mySome = function(array, prime){
+    let primes = [];
+    for(let i = 0; i < array.length; i++){
+        if(prime(array[i])){
+            primes.push(array[i]);
+        }
+    }
+    return primes;
 };
 
 // EVERY //
-Array.prototype.myEvery = function() {
-
+// Thiss function checks the numbers in the argument array
+// If one of the numbers in the array is negative, the function return false.
+let checkNumbers = function(number){
+    return number >= 0 ? 1 : 0;
+};
+let myEvery = function(array, positiveNumbers){
+    for(let i = 0; i < array.length; i++){
+        if(!checkNumbers(array[i])){
+            return false;
+        }
+    }
+    return true;
 };
 
 // REDUCE //
-Array.prototype.myReduce = function() {
-
+//Array.prototype.myReduce = function() {
+let reduce = function(array){
+    let sum = 0;
+    for(let i = 0; i < array.length; i++){
+        sum += array[i];
+    }
+    return sum;
 };
 
 // INCLUDES //
