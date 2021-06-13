f6a0ecb69aad1426c94b2896076ba9125ec2d241
Brang Mai <brangmai100@gmail.com>
2021-06-13 13:42:00 -0400

    Implemented more functions to 'Assignment02.js' file

--- Assignment02.js
+++ Assignment02.js
@@ -1,32 +1,41 @@
 
-/* FOR EACH 
- * This function takes an array of integer and a funcion to print out as arguments
- * Then prints all the elements of the array
+/**
+ * Name : Brng Mai
+ * Assignment : #2
+ * Course : Practical Web Design, Summer 2021
  */
-let forEach = function(array, operation){
+
+/* 
+ * FOR EACH 
+ * This function takes an array of integer and a funcion to print out as arguments
+ * Then prints all the elements of the array by callback function, named "operation".
+ */
+const forEach = function(array, operation){
     for(let i = 0; i < array.length; i++){
         operation(i);
     }
 };
-forEach([11, 22, 33, 44, 55], console.log);
 
-// MAP //
+/*
+ * MAP 
+ * myMap function takes an array and call back function, named "callBack"
+ * The function returns a new mapped array.
+ */
 let square = (number) => number * number;
-let myMap = function(array, callBack){
+const myMap = function(array, callBack){
     let newArray = [];
     for(const element of array){
         newArray.push(callBack(element));
     }
     return newArray;
 };
-console.log(myMap([11,22, 33, 44, 55, 66], square));
 
 /* FILTER 
  * This function takes an integer array as an arguement
  * It then returns even numbers
  */
 let evenNumber = (number) => number % 2 == 0 ? 1 : 0;
-let myFilter = function(array, evenNumber){
+const myFilter = function(array, evenNumber){
     let newArray = [];
     for(let i = 0; i < array.length; i++){
         if(evenNumber(array[i])){
@@ -36,11 +45,11 @@ let myFilter = function(array, evenNumber){
     return newArray;   
 };
 
-console.log(myFilter([11, 22, 33, 44, 55]));
-
-// SOME //
-// mySome function takes a string array and a testing function
-// The functions take word with three characters, then return the new array with three character words
+/* 
+ * SOME 
+ * mySome function takes a string array and a testing function
+ * The functions take word with three characters, then return the new array with three character words 
+ */
 let  wordsWithThreeChars = (word) => word.length === 3 ? 1 : 0;
 let mySome = function(array, wordsWithThreeChars){
     let shortWords = [];
@@ -52,9 +61,11 @@ let mySome = function(array, wordsWithThreeChars){
     return shortWords;
 };
 
-// EVERY //
-// Thiss function checks the numbers in the argument array
-// If one of the numbers in the array is negative, the function return false.
+/* 
+ * EVERY 
+ * Thiss function checks the numbers in the argument array
+ * If one of the numbers in the array is negative, the function return false. 
+ */
 let checkNumbers = (number) => number >= 0 ? 1 : 0;
 let myEvery = function(array, positiveNumbers){
     for(let i = 0; i < array.length; i++){
@@ -65,8 +76,10 @@ let myEvery = function(array, positiveNumbers){
     return true;
 };
 
-// REDUCE //
-//Array.prototype.myReduce = function() {
+/*
+ * REDUCE 
+ * reduce the element of the array: passed an argument
+ */
 let reduce = function(array){
     let sum = 0;
     for(let i = 0; i < array.length; i++){
@@ -75,10 +88,12 @@ let reduce = function(array){
     return sum;
 };
 
-// INCLUDES //
-// if target is matched with an element in array,
-// return true, fasle otherwise.
-let myIncludes = function(array, target){
+/* 
+ * INCLUDES 
+ * if target is matched with an element in array,
+ * return true, fasle otherwise. 
+ */
+const myIncludes = function(array, target){
     for(let i = 0; i < array.length; i++){
         if(target === array[i]){
             return true;
@@ -87,10 +102,12 @@ let myIncludes = function(array, target){
     return false;
 };
 
-// INDEXOF //
-// if target is matched an element of the array,
-// return the index, return -1 otherwise;
-let myIndexOf = function(array, target){
+/* 
+ * INDEXOF 
+ * if target is matched an element of the array,
+ * return the index, return -1 otherwise; 
+ */
+const myIndexOf = function(array, target){
     for(let i = 0; i < array.length; i++){
         if(target === array[i]){
             return i;
@@ -99,17 +116,21 @@ let myIndexOf = function(array, target){
     return -1;
 };
 
-// PUSH //
-// myPush function takes an array and an element as arguments
-// The element is added to the array
+/* 
+ * PUSH 
+ * myPush function takes an array and an element as arguments
+ * The element is added to the array 
+ */
 let myPush = function(array, elementToAdd){
     array.length = array.length + 1;
     array[array.length - 1] = elementToAdd;
 };
 
-// LASTINDEXOF //
-// Return the last index of the array which is matched with the target.
-let myLastIndexOf = function(array, target){
+/* 
+ * LASTINDEXOF //
+ * Return the last index of the array which is matched with the target. 
+ */
+const myLastIndexOf = function(array, target){
     let lastIndex = -1;
     for(let i = 0; i < array.length; i++){
         if(target === array[i]){
@@ -119,12 +140,26 @@ let myLastIndexOf = function(array, target){
     return lastIndex;
 };
 
-// KEYS //
-Object.grabKeys = function() {
-
+/* 
+ * KEYS 
+ * Get all the keys of object, then return the keys of the object. 
+ * */
+const grabKeys = function(object){
+    let keys = [];
+    for(let i = 0; i < object.length; i++){
+        keys.push(i);
+    }
+    return keys;
 };
 
-// VALUES //
-Object.grabValues = function() {
-
+/* 
+ * VALUES
+ * Get all the values of object, then return the values of the object. 
+ */
+const grabValues = function(object){
+    let values = [];
+    for(let i = 0; i < object.length; i++){
+        values.push(object[i]);
+    }
+    return values;
 };
